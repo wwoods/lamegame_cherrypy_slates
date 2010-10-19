a47aae596e75ae7659815965938210fc12b6e72d
WaltW <waltw@LameGameDuo.lamegame.org>
2010-10-19 15:25:21 -0700

    Added __all__ to __init__.py

--- lamegame_cherrypy_slates/__init__.py
+++ lamegame_cherrypy_slates/__init__.py
@@ -2,8 +2,11 @@
 
 Usage: Put tools.lg_slates.on = True in the [global] section in your config file to enable this tool.
 
-tools.lg_slates.storage_type may be specified to change the storage medium (defaults to RamStorage)
+tools.lg_slates.storage_type may be specified to change the storage medium (defaults to 'ram' for RamStorage)
 """
+
+__all__ = [ 'Slate' ]
+__author__ = 'Walt Woods'
 
 import cherrypy
 from .common import *
